723450d1f77d3f895c9ac50c8dcfa62b98a7ad4b
ZulfikarPra <fikarpra@gmail.com>
2022-09-13 07:46:27 +0700

    .env PORT

--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,5 @@ dotenv.config()
 connectDB()
 app.use('/api/word', wordRoutes)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server started on Port ${PORT}`))
